58b2339bb3f33cd69c37a28a1ea1b7f6212c71cb
병준 정 <bill13579@byeongjun-ui-MacBook-Pro.local>
2019-09-11 15:23:06 +0900

    initial commit

---
+++ photo-gallery/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { Camera } from '@ionic-native/camera/ngx';
+import { ErrorHandler } from '@angular/core';
+//import { IonicErrorHandler } from 'ionic-angular';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+//    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
